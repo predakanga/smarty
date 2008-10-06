3c6365aa196df962328d8d9b2680ddf92b748dea
Uwe.Tews <Uwe.Tews@9dce5a81-9a42-0410-99e4-3799d3902e7f>
2008-10-06 20:37:36 +0000

    - added error message on none existing template files.   (U.Tews)
    git-svn-id: http://smarty-php.googlecode.com/svn/branches/Smarty3Alpha@2822 9dce5a81-9a42-0410-99e4-3799d3902e7f

--- libs/sysplugins/internal.displayphp.php
+++ libs/sysplugins/internal.displayphp.php
@@ -1,18 +1,21 @@
 <?php
 
 /**
- * Smarty plugin
- * @package Smarty
- * @subpackage plugins
- */
+* Smarty plugin
+* 
+* @package Smarty
+* @subpackage plugins
+*/
 
 class Smarty_Internal_DisplayPHP extends Smarty_Internal_DisplayBase {
-  
-  public function display($tpl,$tpl_vars) {
-    extract($tpl_vars);
-    include($this->smarty->template_dir . $tpl); 
-  }
-  
-}
+    public function display($tpl, $tpl_vars)
+    {
+        extract($tpl_vars);
+        if (!file_exists($_tpl_filepath)) {
+            $this->smarty->trigger_fatal_error("Template file " . $this->smarty->template_dir . $tpl . " does not exist");
+        } 
+        include($this->smarty->template_dir . $tpl);
+    } 
+} 
 
 ?>
