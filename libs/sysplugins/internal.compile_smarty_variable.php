eb0e5753409c3b664167c4f22bad7eff4925ba03
Uwe.Tews <Uwe.Tews@9dce5a81-9a42-0410-99e4-3799d3902e7f>
2008-10-13 19:13:48 +0000

    newline
    git-svn-id: http://smarty-php.googlecode.com/svn/branches/Smarty3Alpha@2892 9dce5a81-9a42-0410-99e4-3799d3902e7f

--- libs/sysplugins/internal.compile_smarty_variable.php
+++ libs/sysplugins/internal.compile_smarty_variable.php
@@ -4,7 +4,7 @@ class Smarty_Internal_Compile_Smarty_Variable extends Smarty_Internal_CompileBas
     public function execute($args)
     { 
         // $args contains Smarty variable
-        $output = 'echo ' . $args['var'] . '; echo "\n";';
+        $output = 'echo ' . $args['var'] . ';';
 
        // If the template is not evaluated and we have a ncocache section and or a nocache expression
        // make the compiled template to inject the compiled code into the cache file
@@ -13,7 +13,7 @@ class Smarty_Internal_Compile_Smarty_Variable extends Smarty_Internal_CompileBas
                 $output = str_replace("'", "\'", $output);
                 $output = "<?php \$_tmp = '$output'; if (\$this->smarty->caching) echo '<?php '.\$_tmp.'?>'; else eval(\$_tmp);\n?>";
         } else {
-             $output = "<?php ".$output."?>";
+             $output = "<?php ".$output."?>\n";
         }
         // just for debugging
         if ($this->smarty->internal_debugging) {
