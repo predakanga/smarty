f25d4ec646fcd4f71300efc58839545d767bd7d7
Uwe.Tews <Uwe.Tews@9dce5a81-9a42-0410-99e4-3799d3902e7f>
2009-03-14 00:03:41 +0000

    - missing update of file internal.compile_smarty
    git-svn-id: http://smarty-php.googlecode.com/svn/branches/Smarty3Alpha@3043 9dce5a81-9a42-0410-99e4-3799d3902e7f

--- libs/sysplugins/internal.compile_smarty.php
+++ libs/sysplugins/internal.compile_smarty.php
@@ -83,7 +83,7 @@ class Smarty_Internal_Compile_Smarty extends Smarty_Internal_CompileBase {
             case 'global':
                 return "\$_smarty_tpl->smarty->getGlobalVariable($_index[1])->value";
             case 'block':
-               return "'".addcslashes($compiler->template->block_data[trim($_index[1],"'")],"'")."'";
+               return "'".addcslashes($compiler->template->block_data[trim($_index[1],"'")]['source'],"'")."'";
             case 'ldelim':
                 $_ldelim = $this->smarty->left_delimiter;
                 return "'$_ldelim'";
