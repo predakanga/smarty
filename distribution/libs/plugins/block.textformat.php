fb2afdb1a0d6d1f33a1d9fd62d9515d50415d9e9
rodneyrehm <rodneyrehm@9dce5a81-9a42-0410-99e4-3799d3902e7f>
2011-01-10 18:55:30 +0000

    made function textformat UTF-8 compliant
    git-svn-id: http://smarty-php.googlecode.com/svn/branches/rodneyrehm@3925 9dce5a81-9a42-0410-99e4-3799d3902e7f

--- distribution/libs/plugins/block.textformat.php
+++ distribution/libs/plugins/block.textformat.php
@@ -74,7 +74,7 @@ function smarty_block_textformat($params, $content, $template, &$repeat)
         $wrap = 72;
     } 
     // split into paragraphs
-    $_paragraphs = preg_split('![\r\n][\r\n]!', $content);
+    $_paragraphs = preg_split('![\r\n]{2}!', $content);
     $_output = '';
 
     for($_x = 0, $_y = count($_paragraphs); $_x < $_y; $_x++) {
@@ -82,13 +82,18 @@ function smarty_block_textformat($params, $content, $template, &$repeat)
             continue;
         } 
         // convert mult. spaces & special chars to single space
-        $_paragraphs[$_x] = preg_replace(array('!\s+!', '!(^\s+)|(\s+$)!'), array(' ', ''), $_paragraphs[$_x]); 
+        $_paragraphs[$_x] = preg_replace(array('!\s+!u', '!(^\s+)|(\s+$)!u'), array(' ', ''), $_paragraphs[$_x]); 
         // indent first line
         if ($indent_first > 0) {
             $_paragraphs[$_x] = str_repeat($indent_char, $indent_first) . $_paragraphs[$_x];
         } 
         // wordwrap sentences
-        $_paragraphs[$_x] = wordwrap($_paragraphs[$_x], $wrap - $indent, $wrap_char, $wrap_cut); 
+        if (SMARTY_MBSTRING /* ^phpunit */&&empty($_SERVER['SMARTY_PHPUNIT_DISABLE_MBSTRING'])/* phpunit$ */) {
+            require_once(SMARTY_PLUGINS_DIR . 'shared.mb_wordwrap.php');
+            $_paragraphs[$_x] = smarty_mb_wordwrap($_paragraphs[$_x], $wrap - $indent, $wrap_char, $wrap_cut); 
+        } else {
+            $_paragraphs[$_x] = wordwrap($_paragraphs[$_x], $wrap - $indent, $wrap_char, $wrap_cut); 
+        }
         // indent lines
         if ($indent > 0) {
             $_paragraphs[$_x] = preg_replace('!^!m', str_repeat($indent_char, $indent), $_paragraphs[$_x]);
