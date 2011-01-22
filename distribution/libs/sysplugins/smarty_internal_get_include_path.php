221654428e90229394b99d6ad71578da9933af37
rodneyrehm <rodneyrehm@9dce5a81-9a42-0410-99e4-3799d3902e7f>
2011-01-22 17:36:26 +0000

    minor improvement to getIncludePath
    git-svn-id: http://smarty-php.googlecode.com/svn/branches/rodneyrehm@3974 9dce5a81-9a42-0410-99e4-3799d3902e7f

--- distribution/libs/sysplugins/smarty_internal_get_include_path.php
+++ distribution/libs/sysplugins/smarty_internal_get_include_path.php
@@ -20,24 +20,18 @@ class Smarty_Internal_Get_Include_Path {
      */
     public static function getIncludePath($filepath)
     {
-    static $_path_array = null;
+        static $_path_array = null;
 
-    if(!isset($_path_array)) {
-        $_ini_include_path = ini_get('include_path');
-
-        if(strstr($_ini_include_path,';')) {
-            // windows pathnames
-            $_path_array = explode(';',$_ini_include_path);
-        } else {
-            $_path_array = explode(':',$_ini_include_path);
+        if(!isset($_path_array)) {
+            $_ini_include_path = ini_get('include_path');
+            $_path_array = explode(strpos($_ini_include_path,';') !== false ? ';' : ':',$_ini_include_path);
         }
-    }
-    foreach ($_path_array as $_include_path) {
-        if (file_exists($_include_path . DS . $filepath)) {
-            return $_include_path . DS . $filepath;
+        foreach ($_path_array as $_include_path) {
+            if (file_exists($_include_path . DS . $filepath)) {
+                return $_include_path . DS . $filepath;
+            }
         }
-    }
-    return false;
+        return false;
     } 
 } 
 
