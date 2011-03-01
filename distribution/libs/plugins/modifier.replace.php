d7a9b6013309474e6c03ed84cc8e03f4f8fb1bce
rodneyrehm <rodneyrehm@9dce5a81-9a42-0410-99e4-3799d3902e7f>
2011-03-01 18:40:15 +0000

    fixed modifier.replace for missing MBString
    git-svn-id: http://smarty-php.googlecode.com/svn/branches/rodneyrehm@4033 9dce5a81-9a42-0410-99e4-3799d3902e7f

--- distribution/libs/plugins/modifier.replace.php
+++ distribution/libs/plugins/modifier.replace.php
@@ -22,9 +22,12 @@
  */
 function smarty_modifier_replace($string, $search, $replace)
 {
-    // FIXME: (rodneyrehm) mbstring fallback required
-    require_once(SMARTY_PLUGINS_DIR . 'shared.mb_str_replace.php');
-    return smarty_mb_str_replace($search, $replace, $string);
+    if (SMARTY_MBSTRING /* ^phpunit */&&empty($_SERVER['SMARTY_PHPUNIT_DISABLE_MBSTRING'])/* phpunit$ */) {
+        require_once(SMARTY_PLUGINS_DIR . 'shared.mb_str_replace.php');
+        return smarty_mb_str_replace($search, $replace, $string);
+    }
+    
+    return str_replace($search, $replace, $string);
 } 
 
 ?>
