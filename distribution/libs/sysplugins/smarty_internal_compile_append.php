2d5ac4006ff4235200aabe57ff0bcfede8bd64fa
downtown.nerd@gmail.com <downtown.nerd@gmail.com@9dce5a81-9a42-0410-99e4-3799d3902e7f>
2011-06-13 17:06:41 +0000

    found and marked bug
    git-svn-id: http://smarty-php.googlecode.com/svn/branches/Smarty_3_1_cleanup@4139 9dce5a81-9a42-0410-99e4-3799d3902e7f

--- distribution/libs/sysplugins/smarty_internal_compile_append.php
+++ distribution/libs/sysplugins/smarty_internal_compile_append.php
@@ -1,35 +1,40 @@
 <?php
-
 /**
  * Smarty Internal Plugin Compile Append
- * 
+ *
  * Compiles the {append} tag
- * 
+ *
  * @package Smarty
  * @subpackage Compiler
- * @author Uwe Tews 
+ * @author Uwe Tews
  */
+
 /**
  * Smarty Internal Plugin Compile Append Class
+ *
+ * @package Smarty
+ * @subpackage Compiler
  */
 class Smarty_Internal_Compile_Append extends Smarty_Internal_Compile_Assign {
 
     /**
      * Compiles code for the {append} tag
-     * 
+     *
      * @param array $args array with attributes from parser
      * @param object $compiler compiler object
      * @param array $parameter array with compilation parameter
      * @return string compiled code
+     * @todo  Review this section! The argument $parameter is never used.
+     *        But the array $_params is never initialized. Did you mean $_params instead of $parameter?
      */
     public function compile($args, $compiler, $parameter)
     {
         // the following must be assigned at runtime because it will be overwritten in parent class
         $this->required_attributes = array('var', 'value');
         $this->shorttag_order = array('var', 'value');
-        $this->optional_attributes = array('scope','index');
+        $this->optional_attributes = array('scope', 'index');
         // check and get attributes
-        $_attr = $this->_get_attributes($compiler, $args); 
+        $_attr = $this->_get_attributes($compiler, $args);
         // map to compile assign attributes
         if (isset($_attr['index'])) {
             $_params['smarty_internal_index'] = '[' . $_attr['index'] . ']';
@@ -40,10 +45,11 @@ class Smarty_Internal_Compile_Append extends Smarty_Internal_Compile_Assign {
         $_new_attr = array();
         foreach ($_attr as $key => $value) {
             $_new_attr[] = array($key => $value);
-        } 
+        }
         // call compile assign
         return parent::compile($_new_attr, $compiler, $_params);
-    } 
-} 
+    }
+
+}
 
 ?>
