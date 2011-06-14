c7f7cbf585452536b69088b51a4f6f3453f2d013
downtown.nerd@gmail.com <downtown.nerd@gmail.com@9dce5a81-9a42-0410-99e4-3799d3902e7f>
2011-06-14 19:53:58 +0000

    changed as requested by rodneyrehm
    git-svn-id: http://smarty-php.googlecode.com/svn/branches/Smarty_3_1_cleanup@4165 9dce5a81-9a42-0410-99e4-3799d3902e7f

--- distribution/libs/sysplugins/smarty_config_source.php
+++ distribution/libs/sysplugins/smarty_config_source.php
@@ -62,13 +62,13 @@ class Smarty_Config_Source extends Smarty_Template_Source {
     /**
      * <<magic>> Generic setter.
      *
-     * @param string $propertyName valid: content, timestamp, exists
-     * @param mixed  $value        newly assigned value (not check for correct type)
+     * @param string $property_name valid: content, timestamp, exists
+     * @param mixed  $value         newly assigned value (not check for correct type)
      * @throws SmartyException when the given property name is not valid
      */
-    public function __set($propertyName, $value)
+    public function __set($property_name, $value)
     {
-        switch ($propertyName) {
+        switch ($property_name) {
             case 'content':
             case 'timestamp':
             case 'exists':
@@ -76,19 +76,19 @@ class Smarty_Config_Source extends Smarty_Template_Source {
                 break;
 
             default:
-                throw new SmartyException("invalid config property '$propertyName'.");
+                throw new SmartyException("invalid config property '$property_name'.");
         }
     }
 
     /**
      * <<magic>> Generic getter.
      *
-     * @param string $propertyName valid: content, timestamp, exists
+     * @param string $property_name valid: content, timestamp, exists
      * @throws SmartyException when the given property name is not valid
      */
-    public function __get($propertyName)
+    public function __get($property_name)
     {
-        switch ($propertyName) {
+        switch ($property_name) {
             case 'timestamp':
             case 'exists':
                 $this->handler->populateTimestamp($this);
@@ -98,7 +98,7 @@ class Smarty_Config_Source extends Smarty_Template_Source {
                 return $this->content = $this->handler->getContent($this);
 
             default:
-                throw new SmartyException("config property '$propertyName' does not exist.");
+                throw new SmartyException("config property '$property_name' does not exist.");
         }
     }
 
