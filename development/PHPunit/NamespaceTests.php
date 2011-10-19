e290ae6238f47aa73bd711efdb24e09036330628
uwe.tews@googlemail.com <uwe.tews@googlemail.com@9dce5a81-9a42-0410-99e4-3799d3902e7f>
2011-10-19 17:41:02 +0000

    ix PHPunit NamespaceTests
    git-svn-id: http://smarty-php.googlecode.com/svn/branches/Smarty_3_2_DEV@4422 9dce5a81-9a42-0410-99e4-3799d3902e7f

--- development/PHPunit/NamespaceTests.php
+++ development/PHPunit/NamespaceTests.php
@@ -1,12 +1,10 @@
 <?php
 /**
 * Smarty PHPunit tests static class access to constants, variables and methodes
-* 
+*
 * @package PHPunit
-* @author Uwe Tews 
+* @author Uwe Tews
 */
-
-require_once dirname(__FILE__) .'/helpers/namespaced_class.php';
 
 /**
 * class for static class access to constants, variables and methodes tests
@@ -14,66 +12,60 @@ require_once dirname(__FILE__) .'/helpers/namespaced_class.php';
 class NamespaceTests extends PHPUnit_Framework_TestCase {
     public function setUp()
     {
+        require_once dirname(__FILE__) .'/helpers/namespaced_class.php';
+
         $this->smarty = SmartyTests::$smarty;
         SmartyTests::init();
         $this->smarty->disableSecurity();
-    } 
+    }
 
     public static function isRunnable()
     {
-        return true;
-    } 
-    
+        return version_compare(PHP_VERSION, '5.3.0') >= 0;
+    }
+
     public function testConstant()
     {
         $tpl = $this->smarty->createTemplate('eval:{\foo\bar\FOO}');
         $this->assertEquals("CONSTANT", $tpl->fetch());
     }
-    
+
     public function testFunction()
     {
         $tpl = $this->smarty->createTemplate('eval:{$t = \foo\bar\foo()}{$t}');
         $this->assertEquals("FUNCTION", $tpl->fetch());
     }
-    
+
     public function testClassConstant()
     {
         $tpl = $this->smarty->createTemplate('eval:{\foo\bar\Baz::FOO}');
         $this->assertEquals("CONSTANT", $tpl->fetch());
     }
-    
+
     public function testClassStatic()
     {
         $tpl = $this->smarty->createTemplate('eval:{\foo\bar\Baz::$FOO}');
         $this->assertEquals("STATIC", $tpl->fetch());
     }
-    
+
     public function testClassFunction()
     {
         $tpl = $this->smarty->createTemplate('eval:{\foo\bar\Baz::foo()}');
         $this->assertEquals("FUNCTION", $tpl->fetch());
     }
-    
+
     public function testInstanceof()
     {
         $tpl = $this->smarty->createTemplate('eval:{if $object instanceof \foo\bar\Baz}yeah!{/if}');
-        $tpl->assign('object', new \foo\bar\Baz());
+        $class = '\foo\bar\Baz';
+        $tpl->assign('object', new $class());
         $this->assertEquals("yeah!", $tpl->fetch());
-        
+
         $tpl = $this->smarty->createTemplate('eval:{if $object instanceof \'\foo\bar\Baz\'}yeah!{/if}');
-        $tpl->assign('object', new \foo\bar\Baz());
+        $class = '\foo\bar\Baz';
+        $tpl->assign('object', new $class());
         $this->assertEquals("yeah!", $tpl->fetch());
     }
-} 
-
-class mystaticclass {
-    const STATIC_CONSTANT_VALUE = 3;
-    public static $static_var = 5;
-    
-    static function square($i)
-    {
-        return $i*$i;
-    } 
-} 
+}
 
 ?>
